d4f4686c91ee81e1e7571f66527a1c6166853a7f
Ulysse Carion <ulyssecarion@gmail.com>
2014-04-27 18:07:14 -0700

    Add tests for the constructor.

--- spec/winnow_spec.rb
+++ spec/winnow_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
 describe Winnow::Fingerprinter do
+  describe '#initialize' do
+    it 'accepts :guarantee_threshold, :noise_threshold' do
+      fprinter = Winnow::Fingerprinter.new(
+        guarantee_threshold: 0, noise_threshold: 1)
+      expect(fprinter.guarantee_threshold).to eq 0
+      expect(fprinter.noise_threshold).to eq 1
+    end
+
+    it 'accepts :t and :k' do
+      fprinter = Winnow::Fingerprinter.new(t: 0, k: 1)
+      expect(fprinter.guarantee_threshold).to eq 0
+      expect(fprinter.noise_threshold).to eq 1
+    end
+  end
+
   describe '#fingerprints' do
     it 'hashes strings to get keys' do
       # if t = k = 1, then each character will become a fingerprint
