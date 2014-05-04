f53324c9c5d25fc696e456be4261b2b67e3d3fde
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-04 14:12:23 -0700

    Update matcher_spec to expect hashes too.
    Matcher now returns a hash of values to match data.

--- spec/matcher_spec.rb
+++ spec/matcher_spec.rb
@@ -2,48 +2,41 @@ require 'spec_helper'
 
 describe Winnow::Matcher do
   describe '#find_matches' do
-    fprint1 = [
-      Winnow::Fingerprint.new(0, Winnow::Location.new(0, 0)),
-      Winnow::Fingerprint.new(1, Winnow::Location.new(1, 1)),
-      Winnow::Fingerprint.new(1, Winnow::Location.new(2, 2))
-    ]
+    fprint1 = {
+      0 => [Winnow::Location.new(0, 0)],
+      1 => [Winnow::Location.new(1, 1), Winnow::Location.new(2, 2)],
+    }
 
-    fprint2 = [
-      Winnow::Fingerprint.new(0, Winnow::Location.new(3, 3)),
-      Winnow::Fingerprint.new(1, Winnow::Location.new(4, 4)),
-      Winnow::Fingerprint.new(3, Winnow::Location.new(5, 5))
-    ]
+    fprint2 = {
+      0 => [Winnow::Location.new(3, 3)],
+      1 => [Winnow::Location.new(4, 4)],
+      3 => [Winnow::Location.new(5, 5)]
+    }
 
     matches = Winnow::Matcher.find_matches(fprint1, fprint2)
 
-    it 'returns an array of match data' do
-      expect(matches.first).to be_a(Winnow::MatchDatum)
+    it 'returns a hash of match data' do
+      expect(matches[0]).to be_a(Winnow::MatchDatum)
     end
 
     it 'reports a match when values are equal' do
-      match = matches.find do |match|
-        match.matches_from_a.first.value == 0
-      end
+      match = matches[0]
 
-      matchloc_a = match.matches_from_a.first.location
-      matchloc_b = match.matches_from_b.first.location
+      matchloc_a = match.matches_from_a.first
+      matchloc_b = match.matches_from_b.first
 
       expect(matchloc_a.line).to eq 0
       expect(matchloc_b.line).to eq 3
     end
 
     it 'reports nothing when there is no match' do
-      match = matches.find do |match|
-        match.matches_from_a.first.value == 3
-      end
+      match = matches[3]
 
       expect(match).to be_nil
     end
 
     it 'reports all matches when multi matches' do
-      match = matches.find do |match|
-        match.matches_from_a.first.value == 1
-      end
+      match = matches[1]
 
       expect(match.matches_from_a.length).to eq 2
       expect(match.matches_from_b.length).to eq 1
